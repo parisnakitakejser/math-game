e4fd149e8c80deffeb9a7192009f0d15d8a05e84
Paris Nakita Kejser <parisnakitakejser@icloud.com>
2015-05-01 12:52:43 +0200

    (new) starting the game developer
    This commit have main menu ready, and prepare for next step in math-game.

--- math-game/main.c
+++ math-game/main.c
@@ -1,15 +1,139 @@
 //
 //  main.c
-//  math-game
+//  mathGame
 //
 //  Created by Paris Nakita Kejser on 01/05/15.
 //  Copyright (c) 2015 Paris Nakita Kejser. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int menu_number;
+
+int menuMain(void);
+void menuStartNewGame(void);
+void menuGameSettings(void);
+void menuHiscore(void);
+
+/*
+ * Starting the program and handle inputs.
+ */
+int main(void)
+{
+    menuMain();
 }
+
+/*
+ * Build main menu for terminal math-game.
+ */
+int menuMain(void)
+{
+    system("clear");
+    printf("[1] Start new game\n");
+    printf("[2] Game settings\n");
+    printf("[3] Hiscore\n");
+    printf("\n");
+    printf("[0] Exit math-game game\n");
+    scanf("%d", &menu_number);
+    
+    // If the player want to exit the game.
+    if ( menu_number == 0 )
+    {
+        return 0;
+    }
+    // If the player want to start new game
+    else if ( menu_number == 1 )
+    {
+        menuStartNewGame();
+        return 1;
+    }
+    // If the player want game settings menu
+    else if ( menu_number == 2 )
+    {
+        menuGameSettings();
+        return 1;
+    }
+    // If the play what hiscore menu
+    else if ( menu_number == 3 )
+    {
+        menuHiscore();
+        return 1;
+    }
+    // If player press a menu number there not exist.
+    else
+    {
+        main();
+        return 1;
+    }
+}
+
+/*
+ * Build menu to start new game
+ */
+void menuStartNewGame(void)
+{
+    system("clear");
+    printf("# Start new game #\n");
+    printf("\n");
+    printf("None menu to select\n");
+    printf("\n");
+    printf("[0] Back to main menu\n");
+    
+    scanf("%d", &menu_number);
+    
+    // If player want go back to main menu
+    if ( menu_number == 0 )
+        menuMain();
+    
+    // If player press a menu number there not exists.
+    else
+        menuStartNewGame();
+}
+
+/*
+ * Build menu to game settings
+ */
+void menuGameSettings(void)
+{
+    system("clear");
+    printf("# Game settings #\n");
+    printf("\n");
+    printf("None menu to select\n");
+    printf("\n");
+    printf("[0] Back to main menu\n");
+    
+    scanf("%d", &menu_number);
+    
+    // If player want go back to main menu
+    if ( menu_number == 0 )
+        menuMain();
+    
+    // If player press a menu number there not exists.
+    else
+        menuGameSettings();
+}
+
+/*
+ * Build menu to hiscore
+ */
+void menuHiscore(void)
+{
+    system("clear");
+    printf("# Hiscore #\n");
+    printf("\n");
+    printf("None menu to select\n");
+    printf("\n");
+    printf("[0] Back to main menu\n");
+    
+    scanf("%d", &menu_number);
+    
+    // If player want go back to main menu
+    if ( menu_number == 0 )
+        menuMain();
+    
+    // If player press a menu number there not exists.
+    else
+        menuHiscore();
+}
+
